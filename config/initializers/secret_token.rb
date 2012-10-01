8c890be9cfaffe25a03a200fc0eb880b13d94b39
Matteo Collina <matteo.collina@gmail.com>
2012-10-01 15:19:29 +0200

    Added post scaffold.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Courseweblog::Application.config.secret_token = 'c1a32e303750ff42b4106669c7ca5b681022ce108569d49e778b818a49d7f191da3bff07ef7390046ef569e35d9021d1845c572b324e4d3dc45ddd702abb261d'
