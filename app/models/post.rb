d12c9ae3026a1778820ab91d1b301be4cecc9f7e
Matteo Collina <matteo.collina@gmail.com>
2012-10-06 07:50:18 +0200

    Added the association between categories and posts.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ActiveRecord::Base
-  attr_accessible :description, :title
+  attr_accessible :description, :title, :category_id
+
+  validates_presence_of :description, :title, :category_id
+
+  belongs_to :category
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121006052227) do
+ActiveRecord::Schema.define(:version => 20121006053128) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(:version => 20121006052227) do
     t.text     "description"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.integer  "category_id"
   end
 
 end
